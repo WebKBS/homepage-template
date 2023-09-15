4312555ec64ea17e629f45491977f8e0c35a4b9b
강범수 <kbs21c1987@gmail.com>
2023-09-15 16:03:26 +0900

    변경사항적용

--- src/components/Navi.tsx
+++ src/components/Navi.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 function Navi() {
   return (
-    <nav className="flex gap-4">
-      <Link href="/">회사소개</Link>
-      <Link href="/">기술소개</Link>
-      <Link href="/">제품인증</Link>
-      <Link href="/">게시판</Link>
+    <nav className="flex">
+      <Link href="/" className="py-5 px-2.5 font-medium w-20">회사소개</Link>
+      <Link href="/" className="py-5 px-2.5 font-medium w-20">기술소개</Link>
+      <Link href="/" className="py-5 px-2.5 font-medium w-20">제품인증</Link>
+      <Link href="/" className="py-5 px-2.5 font-medium w-20">게시판</Link>
     </nav>
   );
 }
