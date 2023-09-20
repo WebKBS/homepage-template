37e7e4178a00052126c7857ac63cd3e627aedb13
강범수 <kbs21c1987@gmail.com>
2023-09-20 17:21:16 +0900

    네비게이션 모바일 PC 분리 필요

--- src/components/navigator/NaviItem.tsx
+++ src/components/navigator/NaviItem.tsx
@@ -1,23 +1,43 @@
-import {menuItems} from '@/data/navigation';
-import Link from 'next/link';
+'use client';
 
+import {useState} from "react";
+import {menuItems} from "@/data/navigation";
+import Link from "next/link";
+import styles from './NaviItem.module.css';
 
 function NaviItem({onClick}: { onClick?: () => void }) {
+    const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+    const toggleMenu = (index: number) => {
+        if (openIndex === index) {
+            // 클릭한 메뉴가 이미 열려 있는 경우 닫는다
+            setOpenIndex(null);
+        } else {
+            // 클릭한 메뉴 오픈
+            setOpenIndex(index);
+        }
+    };
+
     return (
         <>
             {menuItems.map((menuItem, index) => (
                 <li key={index}>
                     <div>
-                        <button type="button">{menuItem.title}</button>
+                        <button
+                            type="button"
+                            onClick={() => toggleMenu(index)} // 메뉴 클릭 시 토글 함수 호출
+                        >
+                            {menuItem.title}
+                        </button>
                     </div>
-                    <ul>
-                        <li>
-                            {menuItem.links.map((link, linkIndex) => (
-                                <Link key={linkIndex} href={link.href} onClick={onClick}>
+                    <ul className={openIndex === index ? styles.close : styles.open}>
+                        {menuItem.links.map((link, linkIndex) => (
+                            <li key={linkIndex}>
+                                <Link href={link.href} onClick={onClick}>
                                     {link.text}
                                 </Link>
-                            ))}
-                        </li>
+                            </li>
+                        ))}
                     </ul>
                 </li>
             ))}
